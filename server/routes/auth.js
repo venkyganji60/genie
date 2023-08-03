a67a19b2b6b72f048b9bb86fb75d24f07ef49a9a
venkyganji60 <135597770+venkyganji60@users.noreply.github.com>
2023-08-03 15:46:04 +0530

    share on github

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -4,53 +4,47 @@ import axios from "axios";
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-    try{
-        const {username, password} = req.body;
+  try {
+    const { username, password } = req.body;
 
-        const chatEngineResponse = await axios.get(
-            "https://api.chatengine.io/users/me",
-            {
-                headers: { 
-                  "Project-ID": process.env.PROJECT_ID,
-                  "User-Name": username,
-                   "User-Secret": password,
+    const chatEngineResponse = await axios.get(
+      "https://api.chatengine.io/users/me",
+      {
+        headers: {
+          "Project-ID": process.env.PROJECT_ID,
+          "User-Name": username,
+          "User-Secret": password,
+        },
+      }
+    );
 
-              },
-            }
-        );
-
-      res.status(200).json({  response: chatEngineResponse.data });
-    } catch (error) {
-        console.error("error", error);
-        res.status(500).json({ error: error.message });            
-        
-    }
-
+    res.status(200).json({ response: chatEngineResponse.data });
+  } catch (error) {
+    console.error("error", error);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 router.post("/signup", async (req, res) => {
-    try{
-       const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-       const chatEngineResponse = await axios.post(
-        "https://api.chatengine.io/users/",
-        {
-            username: username,
-            secret: password
-        },
-        {
-            headers: { "Private-Key": process.env.PRIVATE_KEY },
-        }
-       );
+    const chatEngineResponse = await axios.post(
+      "https://api.chatengine.io/users/",
+      {
+        username: username,
+        secret: password,
+      },
+      {
+        headers: { "Private-Key": process.env.PRIVATE_KEY },
+      }
+    );
 
-
-      res.status(200).json({ response: chatEngineResponse.data });
-    } catch (error) {
-        console.error("error", error.message);
-        res.status(500).json({ error: error.message });            
-        
-    }
-
+    res.status(200).json({ response: chatEngineResponse.data });
+  } catch (error) {
+    console.error("error", error.message);
+    res.status(500).json({ error: error.message });
+  }
 });
 
 export default router;
