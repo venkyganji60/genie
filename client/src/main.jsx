6312e5789025cc1784a602b6a15d299b65144c48
venkyganji60 <135597770+venkyganji60@users.noreply.github.com>
2023-08-04 09:32:02 +0530

    share on github

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -8,14 +8,14 @@ import { setupListeners } from '@reduxjs/toolkit/query';
 import { api } from '@/state/api';
 
 export const store = configureStore({
-    reducer: {[api.reducerPath] : api.reducer},
+    reducer: { [api.reducerPath]: api.reducer },
     middleware: (getDefault) => getDefault().concat(api.middleware)
 });
 setupListeners(store.dispatch);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-<Provider store={store}>
-   <App />
-</Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
